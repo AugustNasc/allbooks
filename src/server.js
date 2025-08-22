deefcffbeabfbc7bccaf1e53ba0bc69cee4f0c7a
AugustNasc <130488659+AugustNasc@users.noreply.github.com>
2025-08-22 20:48:53 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const router = jsonServer.router('./src/data/database.json')
 const publicRoutes = require('./routes/publicRoutes')
 const authenticationMiddleware = require('./middleware/authenticationMiddleware')
 
+//rotas test conflito merge
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
