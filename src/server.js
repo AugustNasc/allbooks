0811ef4443776185e43ca4835881b4aacdbf040b
August <augusto.nascimentto2@gmail.com>
2025-08-22 21:01:46 -0300

    Atualização do Server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,15 @@
-qqqqqqqqqqqqqconst bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('./src/data/database.json')
 const publicRoutes = require('./routes/publicRoutes')
 const authenticationMiddleware = require('./middleware/authenticationMiddleware')
 
-//rotas test conflito merge
-
-
-//e agora?
-
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
 server.use(jsonServer.defaults());
 
 server.use('/public', publicRoutes) //rotas na porta 8000
-
-//novo conflito
 
 server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddleware);
 
